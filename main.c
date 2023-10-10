bcbead44ede897cb48addbf49ca50dd1cea7407f
lgandari <lgandaf@gmail.com>
2023-10-10 19:19:06 +0200

    add : main.c

---
+++ main.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lganda <lgandari@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 19:01:00 by lganda            #+#    #+#             */
+/*   Updated: 2023/10/10 19:18:35 by lganda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "ft_printf.h"
+# include <stdio.h>
+
+int	main(void)
+{
+	printf("\033[32m##	TEST ft_printf\033[0m\n\n");
+
+	printf("\033[32mImprimir un string\033[m\n");
+	printf("Re: %s", "Hello world!\n");
+	ft_printf("Cpy: %s", "Hello World!\n");
+
+	printf("\033[32mImprimir un puntero\033[m\n");
+	ft_printf("Cpy: %p\n", 0xFFF);
+	//
+	
+	printf("\033[32mImprimir un numero decimal\033[m\n");
+	printf("Re: %d\n", 42);
+	ft_printf("Cpy: %d\n", 42);
+
+	printf("\033[32mImprimir un entero en base 10\033[m\n");
+	printf("Re: %i\n", 42);
+	ft_printf("Cpy: %i\n", 42);
+
+	printf("\033[32mImpimir un entero decimal sin signo\033[m\n");
+	printf("Re: %u\n", 42);
+	ft_printf("Cpy: %u\n", 42);
+
+	printf("\033[32mImprimir un numero hexadecimal en minusculas\033[m\n");
+	printf("Re: %x\n", 62253);
+	ft_printf("Cpy: %x\n", 62253);
+
+	printf("\033[32mImprimir un numero hexadecimal en mayusculas\033[m\n");
+	printf("Re: %X\n", 62253);
+	ft_printf("Cpy: %X\n", 62253);
+
+	printf("\033[32mImpimir simbolo de porcentaje\033[m\n");
+	printf("Re: %%\n");
+	ft_printf("Cpy: %%\n");
+
+	return (0);
+}
